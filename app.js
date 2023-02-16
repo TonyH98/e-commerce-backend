7f4659a670b300ac126eb1c2f5de4f84d71fbfad
TonyH98 <tonyhoang@pursuit.org>
2023-02-16 13:41:47 -0500

    Stripe now in the backend

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+
+const stripe = require("stripe")(process.env.STRIPE_KEY)
 const cors = require('cors')
 const app = express()
+
 
 const products = require("./controller/ProductController")
 
@@ -11,25 +14,38 @@ app.use(express.json())
 
 app.use("/products", products)
 
-// app.use("/products", (req , res , next) => {
-//     const filters = req.query;
-
-//     const filterProduct = products.filter(product => {
-//         let valid = true
-//         for(key in filters){
-//             valid = valid && product[key] == filters[key]
-//         }
-//         return valid
-//     })
-
-//     res.send(filterProduct)
-// })
-
 
 
 app.get("/", (req , res) => {
     res.send("Welcome to the E-Commerce App")
 })
+
+app.post("/payment", ( req , res ) => {
+    const { product , token  } = req.body;
+    
+
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: product.name,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        } , {})
+    })
+
+})
+
 
 
 app.get("*", (req , res) => {
