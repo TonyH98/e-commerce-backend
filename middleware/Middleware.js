888f5ee2f35dd97b793534cf3390fe19f575485a
TonyH98 <tonyhoang@pursuit.org>
2023-03-21 16:09:33 -0400

    Validate Email Added

--- middleware/Middleware.js
+++ middleware/Middleware.js
@@ -1,5 +1,8 @@
 const passwordRequirements = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[0-9a-zA-Z@$!%*?&]{8,}$/;
 
+const validator = require('email-validator')
+
+
 function checkPassword(req, res, next) {
   const { password } = req.body;
   if (!password.match(passwordRequirements)) {
@@ -8,4 +11,16 @@ function checkPassword(req, res, next) {
   next();
 }
 
-module.exports = {checkPassword}
+
+function checkEmail(req , res , next){
+  const email = req.body.email;
+
+  if(!validator.validate(email)){
+    return res.status(400).json({ error: 'Invalid email address' });
+  }
+  next()
+}
+
+
+
+module.exports = {checkPassword, checkEmail}
