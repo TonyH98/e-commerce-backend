ce917f9e16fa22c4649349ae7ff55c905f65b307
TonyH98 <tonyhoang@pursuit.org>
2023-03-30 12:35:15 -0400

    Stripe Payment Successfully Made in Backend

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")("sk_test_51McALmHgd5U2y6vdLJiKvnZq8wWKDvkf3LocRNeV3zVlUwUT0qu9DjPcMtVBIPymxhfvNTQTdbWtdl8ChFKC4oD500pYsmEHeG")
 const cors = require('cors')
 const app = express()
 
@@ -38,30 +38,41 @@ app.post('/logout', isAuthenticated, (req , res) => {
 
 
 app.post('/create-checkout-session', async (req, res) => {
-    const items = req.body.items; // assume that items is an array of objects containing product_id and quantity
-  
-    // fetch the price for each item from Stripe API
-    const lineItems = await Promise.all(
-      items.map(async (item) => {
-        const price = await stripe.prices.retrieve(item.product_id);
-        return {
-          price: price.id,
-          quantity: item.quantity,
-        };
-      })
-    );
-  
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: lineItems,
-      mode: 'payment',
-      success_url: `${PORT}?success=true`,
-      cancel_url: `${PORT}?canceled=true`,
-    });
-  
-    res.json({ id: session.id });
+  const { items } = req.body;
+  console.log("Request body:", req.body);
+  // Map the items array to create an array of line items for the Stripe checkout session
+  const lineItems = items.map((item) => {
+    // Check if item.price is a valid number
+    const unitAmount = Math.round(parseFloat(item.price) * 100);
+    
+    return {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.product_name,
+          images: [item.image],
+        },
+        unit_amount_decimal: unitAmount
+      },
+      quantity: item.quantity,
+    };
   });
   
+  
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel"
+  });
+
+  res.send(JSON.stringify({
+    url: session.url
+  }))
+
+});
+
 
 
 
